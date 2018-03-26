27c9a3207711bceed5494964ce61d13995721f41
tds@gg.com <stagiaire108@ifap.local>
2018-03-27 10:29:30 +1100

    add code

--- converter/markdown.js
+++ converter/markdown.js
@@ -53,6 +53,14 @@ var parseStrong = function(str) {
   }
   return str;
  }
+var parseCode = function(str) {
+  var codeRegExp = /`{1}(\w+)`{1}/;
+  var stra = [];
+  while ((stra = codeRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<pre>' + stra[1] + '</pre>');
+  }
+  return str;
+}
 
  var parseNewLine = function(str) {
   var newLineRegExp = /(\n)/;
@@ -74,6 +82,7 @@ var markdown = {
     str = parseStrong(str);
     str = parseHorizontaleLine(str);
     str = parseLink(str);
+	str = parseCode(str);
     return str;
   }
 };
